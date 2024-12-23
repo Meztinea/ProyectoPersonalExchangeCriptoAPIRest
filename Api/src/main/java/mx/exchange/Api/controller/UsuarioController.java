d1a2aba91c360d938378734e151663b027cf822c
Raquel Martínez <meztinea.dev@gmail.com>
2024-12-23 17:02:13 -0600

    Agrega DTO para crear usuarios
    DTO que utiliza el controller cuando se ejecuta un POST de usuarios

--- Api/src/main/java/mx/exchange/Api/controller/UsuarioController.java
+++ Api/src/main/java/mx/exchange/Api/controller/UsuarioController.java
@@ -1,2 +1,17 @@
-package mx.exchange.Api.controller;public class UsuarioController {
+package mx.exchange.Api.controller;
+
+import mx.exchange.Api.dto.CrearUsuarioDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @PostMapping
+    public void registrarUsuario(@RequestBody CrearUsuarioDTO usuario){
+        System.out.println(usuario);
+    }
 }
