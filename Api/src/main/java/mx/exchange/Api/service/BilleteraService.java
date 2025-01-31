74b7fd5eebba2ad24e062a282221c05729af4888
Raquel Martínez <meztinea.dev@gmail.com>
2025-01-30 19:34:32 -0600

    Agrega BilleteraService e inyecta dependencias

---
+++ Api/src/main/java/mx/exchange/Api/service/BilleteraService.java
@@ -0,0 +1,23 @@
+package mx.exchange.Api.service;
+
+import mx.exchange.Api.repository.BilleteraRepository;
+import mx.exchange.Api.repository.CriptomonedaRepository;
+import mx.exchange.Api.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BilleteraService {
+
+    @Autowired
+    private final BilleteraRepository billeteraRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final CriptomonedaRepository criptomonedaRepository;
+
+    public BilleteraService(BilleteraRepository billeteraRepository, UsuarioRepository usuarioRepository,
+                            CriptomonedaRepository criptomonedaRepository){
+        this.billeteraRepository = billeteraRepository;
+        this.usuarioRepository = usuarioRepository;
+        this.criptomonedaRepository = criptomonedaRepository;
+    }
+}
